3f4845ecb91b93782548b56391bc6ee57645bf1a
Taiki <s.taiki0301@gmail.com>
2024-04-08 01:20:50 +0900

    ログイン機能の追加

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+
   namespace :api do
     namespace :v1 do
       get "health_check", to: "health_check#index"
+      mount_devise_token_auth_for "User", at: "auth"
     end
   end
 end
