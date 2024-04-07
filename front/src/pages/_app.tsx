5dd5551e77d605895070ab0d381c492bd82e28cf
Taiki <s.taiki0301@gmail.com>
2024-04-07 23:47:24 +0900

    mui, emotion, babelの導入導入

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -1,6 +1,25 @@
-import '@/styles/globals.css'
+import { CacheProvider, EmotionCache } from '@emotion/react'
+import CssBaseline from '@mui/material/CssBaseline'
+import { ThemeProvider } from '@mui/material/styles'
 import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import createEmotionCache from '@/styles/createEmotionCache'
+import theme from '@/styles/theme'
+
+const clientSideEmotionCache = createEmotionCache()
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache
+}
+
+export default function MyApp(props: MyAppProps): JSX.Element {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  )
 }
