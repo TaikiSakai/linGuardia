af179d079a407d36da439e000d20b4ec655d041f
Taiki <s.taiki0301@gmail.com>
2024-04-26 18:04:32 +0900

    テスト対応

---
+++ api/spec/models/card_spec.rb
@@ -0,0 +1,58 @@
+require 'rails_helper'
+
+RSpec.describe Card, type: :model do
+  context "factoryのデフォルトに従った時" do
+    subject { create(:card) }
+
+    it "正常にレコードを新規作成できる" do
+      expect { subject }.to change { Card.count }.by(1)
+    end
+  end
+
+  describe "Validations" do
+    subject { card.valid? }
+
+    let(:user) { create(:user) }
+    let(:title) { Faker::Lorem.word }
+    let(:status) { :open }
+    let(:user) { create(:user)}
+    let(:card) { build(:card, title: title, status: status, user: user) }
+
+    context "すべての値が正常な時" do
+      it "バリデーションに成功する" do
+        expect(subject).to be_truthy
+      end
+    end
+
+    context "ステータスが公開かつ、タイトルが空の時" do
+      let(:title) { "" }
+
+      it "エラーメッセージが返る" do
+        expect(subject).to be_falsey
+        expect(card.errors.full_messages).to eq [
+          "タイトルを入力してください",
+          "タイトルは1文字以上で入力してください"
+          ]
+      end
+    end
+
+    context "ステータスが公開済みかつ、ステータスが空の時" do
+      let(:status) { "" }
+      
+      it "エラーメッセージが返る" do
+        expect(subject).to be_falsey
+        expect(card.errors.full_messages).to eq ["ステータスを入力してください"]
+      end
+    end
+
+    context "ユーザーが同じタイトルのcardを登録していた時" do
+      let!(:existing_card) { create(:card, user: user) }
+      let(:title) { existing_card.title }
+
+      it "エラーメッセージが返る" do
+        expect(subject).to be_falsey
+        expect(card.errors.full_messages).to eq ["タイトルはすでに存在します"]
+      end
+    end
+  end
+end
