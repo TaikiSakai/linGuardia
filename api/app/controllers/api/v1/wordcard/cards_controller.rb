f8c3162e9dabb147d30deac10df04dabbf30c4fe
Taiki <s.taiki0301@gmail.com>
2024-04-26 18:05:30 +0900

    cardコントローラーのエラー対応

--- api/app/controllers/api/v1/wordcard/cards_controller.rb
+++ api/app/controllers/api/v1/wordcard/cards_controller.rb
@@ -2,22 +2,22 @@ class Api::V1::Wordcard::CardsController < Api::V1::BaseController
   before_action :authenticate_user!
 
   def index
-    cards = current_user.cards.includes(:user)
-    render json: { card_list: cards }, status: :ok
-  end
+    cards = current_user.cards.all
 
-  def show
-    card = current_user.cards.find_by!(uuid: params[:uuid])
-    render json: { card: }, status: :ok
+    if cards.empty?
+      render json: { message: "単語帳が登録されていません" }, status: :ok
+    else
+      render json: { cards: cards } , status: :ok
+    end
   end
 
   def create
     card = current_user.cards.new(card_params)
 
     if card.save
-      render json: { message: card.title }, status: :ok
+      render json: { message: "単語帳を作成しました" }, status: :ok
     else
-      render json: { error: card.errors,
+      render json: { errors: card.errors.full_messages,
                      message: "単語帳を作成できません" }, status: :bad_request
     end
   end
@@ -28,13 +28,14 @@ class Api::V1::Wordcard::CardsController < Api::V1::BaseController
     if card.update(card_params)
       render json: { message: "更新しました" }, status: :ok
     else
-      render json: { error: card.errors,
+      render json: { errors: card.errors,
                      message: "更新に失敗しました" }, status: :bad_request
     end
   end
 
-  def destroy
+  def destroy    
     card = current_user.cards.find_by!(uuid: params[:uuid])
+
     card.destroy!
     render json: { message: "削除しました" }, status: :ok
   end
