2a05765c789c6549dce3b4a8e3c712c81250d0ed
Taiki <s.taiki0301@gmail.com>
2024-05-01 12:23:03 +0900

    単語編集用モーダル

---
+++ front/src/components/ModalState.ts
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+const useModal = () => {
+  const [open, setOpen] = useState<boolean>(false)
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
+
+  return [open, handleOpen, handleClose] as [boolean, () => void, () => void]
+}
+
+export default useModal
