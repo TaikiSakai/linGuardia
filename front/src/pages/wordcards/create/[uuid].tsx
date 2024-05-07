4287c70d37ae8685cbecc1a57fe15ec7a341d166
Taiki <s.taiki0301@gmail.com>
2024-05-08 00:07:20 +0900

    単語登録画面 BottomMenu対応

--- front/src/pages/wordcards/create/[uuid].tsx
+++ front/src/pages/wordcards/create/[uuid].tsx
@@ -1,7 +1,11 @@
-import { Box, Button, Container, Grid, TextField, Stack } from '@mui/material';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import { Box, Button, Container, Grid, TextField, Stack, Paper } from '@mui/material';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import axios, { AxiosResponse, AxiosError } from 'axios';
 import camelcaseKeys from 'camelcase-keys';
 import type { NextPage } from 'next';
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { useState, createContext } from 'react';
 import { SubmitHandler, useForm, Controller } from 'react-hook-form';
@@ -22,7 +26,7 @@ const AddPage: NextPage = () => {
   const { handleSubmit, control, reset } = useForm<VocabularyData>();
   const [open, handleOpen, handleClose] = useModal(reset);
 
-  // 入力値のインデックス管理 
+  // 入力値のインデックス管理
   // カウンターを使用することで、入力値を削除した場合でもインデックスが重複しない
   const [count, setCount] = useState<number>(0);
 
@@ -59,6 +63,7 @@ const AddPage: NextPage = () => {
       css={styles.pageMinHeight}
       sx={{
         backgroundColor: '#e6f2ff',
+        pb: 7,
       }}
     >
       <Container maxWidth="md" sx={{ pt: 6, pb: 6 }}>
@@ -86,7 +91,6 @@ const AddPage: NextPage = () => {
             </DeleteInputContext.Provider>
           </AddInputContext.Provider>
         </InputContext.Provider>
-        <Button onClick={handleOpen}>test</Button>
         <Button onClick={() => console.log(inputValues)}>show inputvalues</Button>
       </Container>
       <ModalCard title="単語新規登録" open={open} handleClose={handleClose}>
@@ -129,9 +133,26 @@ const AddPage: NextPage = () => {
               />
             )}
           />
-          <Button variant="contained" type="submit">追加</Button>
+          <Button variant="contained" type="submit">
+            追加
+          </Button>
         </Stack>
       </ModalCard>
+      <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }}>
+        <BottomNavigation showLabels>
+          <Box display="flex" alignItems="center">
+            <Link href="/wordcards">
+              <Button sx={{ width: 100 }}>キャンセル</Button>
+            </Link>
+          </Box>
+          <Button onClick={handleOpen}>
+            <AddCircleIcon />
+          </Button>
+          <Box display="flex" alignItems="center">
+            <Button sx={{ width: 100 }}>登録</Button>
+          </Box>
+        </BottomNavigation>
+      </Paper>
     </Box>
   );
 };
