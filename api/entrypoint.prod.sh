d1a36340d5216d99285622582413f1985110b327
Taiki <s.taiki0301@gmail.com>
2024-05-19 18:30:28 +0900

    production用環境の整備

---
+++ api/entrypoint.prod.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+echo "Start entrypoint.prod.sh"
+
+echo "rm -f /myapp/tmp/pids/server.pid"
+rm -f /myapp/tmp/pids/server.pid
+
+# 最初のデプロイのみで使用する
+# echo "bundle exec rails db:create RAILS_ENV=production"
+# bundle exec rails db:create RAILS_ENV=production
+
+# echo "bundle exec rails db:migrate RAILS_ENV=production"
+# bundle exec rails db:migrate RAILS_ENV=production
+
+# echo "bundle exec rails db:seed RAILS_ENV=production"
+# bundle exec rails db:seed RAILS_ENV=production
+
+echo "exec pumactl start"
+bundle exec pumactl start
