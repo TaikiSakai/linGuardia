cb26c252a48ec212358c823dc900d0c8c4a8e583
Taiki <s.taiki0301@gmail.com>
2024-06-15 23:55:38 +0900

    change: ホームのリンク先をダッシュボードに変更

--- front/src/styles/index.ts
+++ front/src/styles/index.ts
@@ -4,4 +4,22 @@ export const styles = {
   pageMinHeight: css({
     minHeight: 'calc(100vh - 57px)',
   }),
+  pageTitle: css({
+    component: 'h3',
+    fontSize: 35,
+    '@media (max-width: 600px)': {
+      fontSize: 30,
+    },
+    fontWeight: 'bold',
+    color: '#000040',
+  }),
+  subTitle: css({
+    component: 'h4',
+    fontSize: 20,
+    '@media (max-width: 600px)': {
+      fontSize: 15,
+    },
+    fontWeight: 'bold',
+    color: '#000040',
+  }),
 };
