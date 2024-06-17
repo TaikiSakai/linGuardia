162470613a785509849bf1d62943105641487c78
Taiki <s.taiki0301@gmail.com>
2024-06-17 23:25:59 +0900

    update: レスポンスメッセージのキーを変更

--- api/app/controllers/api/v1/wordcard/likes_controller.rb
+++ api/app/controllers/api/v1/wordcard/likes_controller.rb
@@ -3,11 +3,11 @@ class Api::V1::Wordcard::LikesController < Api::V1::BaseController
   before_action :set_card
 
   def create
-    card_like = current_user.likes.new(user_id: current_user.id, card_id: @card.id)
+    card_like = current_user.likes.new(card_id: @card.id)
     if card_like.save
       render json: { message: "OK" }, status: :ok
     else
-      render json: { message: card_like.errors.full_messages }, status: :conflict
+      render json: { error: card_like.errors.full_messages }, status: :conflict
     end
   end
 
@@ -17,18 +17,13 @@ class Api::V1::Wordcard::LikesController < Api::V1::BaseController
     if card_like.destroy
       render json: { message: "OK" }
     else
-      render json: { message: card_like.errors.full_messages }
+      render json: { error: card_like.errors.full_messages }
     end
   end
 
   private
-
-    def like_params
-      params.require(:like).permit(:uuid)
-    end
-
     def set_card
       @card = Card.find_by(uuid: params[:card_uuid])
-      render json: { message: "単語帳が見つかりません" }, status: :not_found unless @card
+      render json: { error: "単語帳が見つかりません" }, status: :not_found unless @card
     end
 end
