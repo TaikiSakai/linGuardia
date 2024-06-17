d3f211b457a8473dbe2d0cb2dfbc3935c38aa0b7
Taiki <s.taiki0301@gmail.com>
2024-06-17 11:10:06 +0900

    add: いいね機能追加

--- api/app/serializers/ranked_card_serializer.rb
+++ api/app/serializers/ranked_card_serializer.rb
@@ -1,7 +1,11 @@
 class RankedCardSerializer < ActiveModel::Serializer
-  attributes :uuid, :title, :user_name
+  attributes :uuid, :title, :user_name, :like
 
   def user_name
     object.user.name
+  end
+
+  def like
+    current_user.like?(object)
   end
 end
