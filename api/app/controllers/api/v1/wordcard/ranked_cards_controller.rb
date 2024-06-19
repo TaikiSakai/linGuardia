4c8726e6a216556de40dcf9784500b5060a0587c
Taiki <s.taiki0301@gmail.com>
2024-06-19 23:57:03 +0900

    update: アクセス数で高順にソートする処理を追加

--- api/app/controllers/api/v1/wordcard/ranked_cards_controller.rb
+++ api/app/controllers/api/v1/wordcard/ranked_cards_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::Wordcard::RankedCardsController < Api::V1::BaseController
   def index
     # ページネーション対応
     # 検索機能対応
-    cards = Card.where(status: "open").includes(:user)
+    cards = Card.where(status: "open").includes(:user).order(number_of_access: "DESC").limit(5)
     render json: cards, each_serializer: RankedCardSerializer, status: :ok
   end
 end
