90fceb84750afd87a0051dd42fe023da0ca3560a
Taiki <s.taiki0301@gmail.com>
2024-06-23 23:26:30 +0900

    update: アプリケーション共通CSSを追加

--- front/src/styles/index.ts
+++ front/src/styles/index.ts
@@ -28,4 +28,41 @@ export const styles = {
     fontWeight: 'bold',
     color: '#000040',
   }),
+  textField: css({
+    '& .MuiOutlinedInput-root': {
+      '& fieldset': {
+        borderColor: '#000060', // 通常時の枠の色
+      },
+      '&:hover fieldset': {
+        borderColor: '#000060', // ホバー時の枠の色
+      },
+      '&.Mui-focused fieldset': {
+        borderColor: '#000060', // フォーカス時の枠の色
+      },
+    },
+  }),
+  modalTitle: css({
+    component: 'h3',
+    fontSize: '30px',
+    '@media (max-width: 600px)': {
+      fontSize: '25px',
+    },
+    fontWeight: 'bold',
+    color: '#000040',
+  }),
+  modalText: css({
+    component: 'p',
+    fontSize: '15px',
+    '@media (max-width: 600px)': {
+      fontSize: '15px',
+    },
+    fontWeight: 'bold',
+    color: '#000040',
+  }),
+  styledButton: css({
+    backgroundColor: '#000060',
+    borderRadius: 8,
+    transition: 'all 150ms ease',
+    border: '1px solid #01015c',
+  }),
 };
