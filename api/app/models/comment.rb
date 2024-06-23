0a1a42052912a2d5c1a977bf118ddd71967ffc88
Taiki <s.taiki0301@gmail.com>
2024-06-23 17:22:29 +0900

    update: コメント文字数制限を変更

--- api/app/models/comment.rb
+++ api/app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :card
 
-  validates :body, presence: true, length: { maximum: 80 }
+  validates :body, presence: true, length: { maximum: 30 }
 end
