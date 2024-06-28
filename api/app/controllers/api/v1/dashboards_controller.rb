446f675a1fe1a28c89aa743f1c9d7037c9e2c6c0
Taiki <s.taiki0301@gmail.com>
2024-06-29 00:22:15 +0900

    delete: ranked_cards_controllerで対応するため削除

--- api/app/controllers/api/v1/dashboards_controller.rb
+++
@@ -1,9 +0,0 @@
-class Api::V1::DashboardsController < Api::V1::BaseController
-  before_action :authenticate_user!
-
-  def index
-    # ランキングで上位10を選出する
-    ranked_cards = Card.where(status: "open").includes(:user).order(number_of_access: "DESC").limit(5)
-    render json: ranked_cards, each_serializer: RankedCardSerializer, status: :ok
-  end
-end
