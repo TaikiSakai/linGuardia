2306ea3626566701681334aafd75bafae5d084e1
Taiki <s.taiki0301@gmail.com>
2024-06-29 00:28:01 +0900

    update: バリデーションエラー発生時のレスポンスを変更

--- api/app/controllers/api/v1/wordcard/cards_controller.rb
+++ api/app/controllers/api/v1/wordcard/cards_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::Wordcard::CardsController < Api::V1::BaseController
   before_action :authenticate_user!
-  before_action :set_card, only: [:show, :update, :destroy]
+  before_action :set_card, only: [:update, :destroy]
 
   def index
     cards = current_user.cards.all.order(created_at: :desc)
@@ -13,23 +13,27 @@ class Api::V1::Wordcard::CardsController < Api::V1::BaseController
   end
 
   def show
-    if @card
-      # 所有者以外のユーザーがアクセスしたら。アクセス数をカウントする
-      @card.count_access_number(@card) unless @card.user == current_user
-      render json: @card, each_serializer: CardSerializer, status: :ok
+    card = Card.find_by(uuid: params[:uuid])
+
+    if card
+      # 所有者以外のユーザーがアクセスしたらアクセス数をカウントする
+      card.count_access_number(card) unless card.user == current_user
+      render json: card, each_serializer: CardSerializer, status: :ok
     end
   end
 
   def create
     card = current_user.cards.new(card_params)
 
-    card.save!
-    render json: {
-      card: card,
-      message: "単語帳を作成しました",
-    }, status: :ok
-  rescue ActiveRecord::RecordInvalid => e
-    render json: { error: e.record.errors.full_messages }, status: :unprocessable_entity
+    if card.save
+      render json: { card: card, message: "単語帳を作成しました" }, status: :ok
+    else
+      render json: { error: card.errors.full_messages }, status: :bad_request
+    end
+
+  # statusのenumに範囲外の値が渡された場合の例外メッセージ
+  rescue ArgumentError
+    render json: { error: "ステータスが無効です" }, status: :bad_request
   end
 
   def update
@@ -39,7 +43,7 @@ class Api::V1::Wordcard::CardsController < Api::V1::BaseController
       render json: { error: @card.errors.full_messages }, status: :bad_request
     end
 
-  # statusのenumに範囲外の値が渡された場合の処理
+  # statusのenumに範囲外の値が渡された場合の例外メッセージ
   rescue ArgumentError
     render json: { error: "ステータスが無効です" }, status: :bad_request
   end
@@ -48,7 +52,7 @@ class Api::V1::Wordcard::CardsController < Api::V1::BaseController
     if @card.destroy
       render json: { message: "単語帳を削除しました" }, status: :ok
     else
-      render json: { error: "単語帳を削除できません" }, status: :internal_server_error
+      render json: { error: @card.errors.full_messages }, status: :internal_server_error
     end
   end
 
@@ -59,7 +63,7 @@ class Api::V1::Wordcard::CardsController < Api::V1::BaseController
     end
 
     def set_card
-      @card = Card.find_by(uuid: params[:uuid])
+      @card = current_user.cards.find_by(uuid: params[:uuid])
       render json: { error: "単語帳が見つかりません" }, status: :not_found unless @card
     end
 end
