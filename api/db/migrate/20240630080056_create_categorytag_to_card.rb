103ca024436ae7f869b44078948b066fca353ae0
Taiki <s.taiki0301@gmail.com>
2024-06-30 17:17:36 +0900

    update: マイグレーション名を変更

--- api/db/migrate/20240630080056_create_categorytag_to_card.rb
+++ api/db/migrate/20240630080056_create_categorytag_to_card.rb
@@ -1,4 +1,4 @@
-class AddCategorytagToCard < ActiveRecord::Migration[7.0]
+class CreateCategorytagToCard < ActiveRecord::Migration[7.0]
   def change
     create_table :categories do |t|
       t.string :name, null: false, unique: true
