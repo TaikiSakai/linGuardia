0349e1d40def0fdb663892caaab3c1add2e6943e
Taiki <s.taiki0301@gmail.com>
2024-06-30 12:54:04 +0900

    update: serializerをCardSerializerに変更

--- api/app/controllers/api/v1/wordcard/ranked_cards_controller.rb
+++ api/app/controllers/api/v1/wordcard/ranked_cards_controller.rb
@@ -5,6 +5,6 @@ class Api::V1::Wordcard::RankedCardsController < Api::V1::BaseController
     # ページネーション対応
     # 検索機能対応
     cards = Card.where(status: "open").includes(:user).order(number_of_access: "DESC").limit(5)
-    render json: cards, each_serializer: RankedCardSerializer, status: :ok
+    render json: cards, each_serializer: CardSerializer, status: :ok
   end
 end
