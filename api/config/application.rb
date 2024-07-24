a19151fcefd65bcdeb6b59a85e8a5ee97aab5d6c
Taiki <s.taiki0301@gmail.com>
2024-07-25 00:58:56 +0900

    update: タイムゾーンを東京に変更

--- api/config/application.rb
+++ api/config/application.rb
@@ -30,7 +30,7 @@ module App
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Tokyo"
     # config.eager_load_paths << Rails.root.join("extras")
 
     config.session_store :cookie_store, key: "_session_api"
