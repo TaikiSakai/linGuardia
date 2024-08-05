4c303dd8da8141d33fcfcfeb60441af6f9204d74
Taiki <s.taiki0301@gmail.com>
2024-08-05 20:20:13 +0900

    update: group_records_by_titleの処理をprepare_responseに統合

--- api/app/service/study_record_service.rb
+++ api/app/service/study_record_service.rb
@@ -4,10 +4,6 @@ class StudyRecordService
     @date_list = date_list
   end
 
-  def group_record_by_title
-    @records.group_by {|record| record.card.title }
-  end
-
   def calculate_word_counts(record)
     word_counts_by_date = record.group_by(&:date).transform_values {|recs| recs.sum(&:word_count) }
     @date_list.map {|date| word_counts_by_date[date] || 0 }
@@ -32,8 +28,8 @@ class StudyRecordService
     @records.where(date: Time.zone.yesterday).pluck(:word_count).sum
   end
 
-  def prepare_response
-    grouped_records = group_record_by_title
+  def group_records_by_title
+    grouped_records = @records.group_by {|record| record.card.title }
 
     grouped_records.map do |card_title, record|
       {
