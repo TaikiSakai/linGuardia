aea91abdff85b2f7d7384d910386f38b3249cae8
Taiki <s.taiki0301@gmail.com>
2024-08-18 15:51:54 +0900

    update: モジュール名を修正

--- api/app/service/openai/request_base.rb
+++ api/app/service/openai/request_base.rb
@@ -1,20 +1,18 @@
-module Openai
-  class RequestBase
-    def initialize(model: "gpt-3.5-turbo", timeout: 30)
-      @model = model
+class Openai::RequestBase
+  def initialize(model: "gpt-3.5-turbo", timeout: 30)
+    @model = model
 
-      @request = Faraday.new(url: "https://api.openai.com") do |req|
-        req.headers["Content-Type"] = "application/json"
-        req.headers["Authorization"] = "Bearer #{Settings.api_key}"
-        req.options[:timeout] = timeout
-        req.adapter Faraday.default_adapter
-      end
+    @request = Faraday.new(url: "https://api.openai.com") do |req|
+      req.headers["Content-Type"] = "application/json"
+      req.headers["Authorization"] = "Bearer #{Settings.api_key}"
+      req.options[:timeout] = timeout
+      req.adapter Faraday.default_adapter
     end
+  end
 
-    def post_request(url: "/", body: {})
-      @request.post(url) {|req| req.body = body }
-    rescue Faraday::TimeoutError
-      raise StandardError, "タイムアウトになりました"
-    end
+  def post_request(url: "/", body: {})
+    @request.post(url) {|req| req.body = body }
+  rescue Faraday::TimeoutError
+    raise StandardError, "タイムアウトになりました"
   end
 end
